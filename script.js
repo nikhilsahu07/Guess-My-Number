6dbec6b0a7ac4ad42daa48b0f458e8a721d634d9
Nikhil Sahu <heynikhil07@gmail.com>
2023-04-17 22:01:59 +0530

    Learned the DOM and DOM manipulation and selecting and manipulating elements

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
 
-const a = 55
+document.querySelector('.score').textContent = 10;
+console.log(document.querySelector('.score').textContent);
+document.querySelector('.highscore').textContent = 8;
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+*/
